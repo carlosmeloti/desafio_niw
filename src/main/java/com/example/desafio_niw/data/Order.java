38f1c50435a4f55de5e0b9d3163a19d29210f29b
Carlos Melo <carlosmeloti@gmail.com>
2026-03-27 10:20:41 +0000

    Annotate `Order` and `OrderItem` entities with JPA annotations

--- src/main/java/com/example/desafio_niw/data/Order.java
+++ src/main/java/com/example/desafio_niw/data/Order.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import java.util.List;
 
 @Entity
+@Data
 public class Order {
 
     @Id
--- src/main/java/com/example/desafio_niw/data/OrderItem.java
+++ src/main/java/com/example/desafio_niw/data/OrderItem.java
@@ -1,16 +1,22 @@
 package com.example.desafio_niw.data;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
 @Data
+@Entity
 public class OrderItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String productId;
+    @Column(nullable = false)
     private BigDecimal price;
+    @Column(nullable = false)
     private Long quantity;
-
 
 
 }
