69cf53b4122f04211a176021c52e30ce58362830
Carlos Melo <carlosmeloti@gmail.com>
2026-03-27 11:08:13 +0000

    Annotate `CustomerController` methods with REST mapping annotations

--- src/main/java/com/example/desafio_niw/controller/CustomerController.java
+++ src/main/java/com/example/desafio_niw/controller/CustomerController.java
@@ -1,15 +1,22 @@
 package com.example.desafio_niw.controller;
 
 import com.example.desafio_niw.data.Customer;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/customers")
 public interface CustomerController {
 
+    @GetMapping
     List<Customer> findAll();
+    @GetMapping("/{id}")
     Customer findById(Long id);
+    @PostMapping
     Customer create(Customer customer);
+    @PutMapping("/{id}")
     Customer update(Long id, Customer customer);
+    @DeleteMapping("/{id}")
     void delete(Long id);
 
 }
