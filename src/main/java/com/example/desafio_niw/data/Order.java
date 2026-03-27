3d16f62802c43552e3544a9f78d73736279b3042
Carlos Melo <carlosmeloti@gmail.com>
2026-03-27 10:18:51 +0000

    Annotate `Order` entity with JPA annotations and configure relationships

--- src/main/java/com/example/desafio_niw/data/Order.java
+++ src/main/java/com/example/desafio_niw/data/Order.java
@@ -1,14 +1,26 @@
 package com.example.desafio_niw.data;
 
 import com.example.desafio_niw.data.enums.OrderStatus;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.List;
-@Data
+
+@Entity
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long  id;
+
+    @Column(nullable = false)
     private String customerId;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "order_id")
     private List<OrderItem> items;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private OrderStatus status;
+
+
 }
